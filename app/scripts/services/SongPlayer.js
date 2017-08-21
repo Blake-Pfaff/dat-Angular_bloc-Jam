16cf4272ec1669b727d568e98d016ee050d86d4d
Thrilldozer <blake.a.pfaff2.com>
2017-08-21 10:15:49 -0500

    adds pause/play and previous button logic

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,10 +1,10 @@
 
 
 (function() {
-    function SongPlayer() {
+    function SongPlayer(Fixtures) {
         var SongPlayer = {};
-
-        var currentSong = null;
+        // @desc sets album data from Fixtures
+        var currentAlbum = Fixtures.getAlbum();
         // /**
         // * @desc Buzz object audio file
         // * @type {Object}
@@ -18,7 +18,7 @@
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
         }
 
         currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -26,32 +26,62 @@
             preload: true
         });
 
-            currentSong = song;
+            SongPlayer.currentSong = song;
         };
 
         var playSong = function() {
             currentBuzzObject.play();
-            currentSong.playing = true;
+            SongPlayer.currentSong.playing = true;
+        };
+        // * @function private, getSongIndex
+        // * @desc gets the index of the current song from the fixtures object
+        // * @param {Object} song
+        var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
         };
 
+        SongPlayer.currentSong = null;
+        // * @function play
+        // * @desc Play current or new song
+        // * @param {Object} song
         SongPlayer.play = function(song) {
             // if another song is selected the if statement will check to for status
-            if (currentSong !== song) {
+            song = song || SongPlayer.currentSong;
+            if (SongPlayer.currentSong !== song) {
 
                 setSong(song);
 
                 playSong();
 
-                } else if (currentSong === song) {
+                } else if (SongPlayer.currentSong === song) {
                     if (currentBuzzObject.isPaused()) {
                         currentBuzzObject.play();
                     }
                 }
         };
-
+        // * @function pause
+        // * @desc Pause current song
+        // * @param {Object} song
         SongPlayer.pause = function(song) {
+          song = song || SongPlayer.currentSong;
             currentBuzzObject.pause();
             song.playing = false;
+        };
+        // * @function previous
+        // * @desc playes the previous song
+        // * @param none
+        SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+
+            if (currentSongIndex < 0) {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            } else {
+              var song = currentAlbum.songs[currentSongIndex];
+              setSong(song);
+              playSong(song);
+            }
         };
 
         return SongPlayer;
@@ -59,5 +89,5 @@
 
      angular
          .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
+         .factory('SongPlayer', ['Fixtures', SongPlayer]);
  })();
